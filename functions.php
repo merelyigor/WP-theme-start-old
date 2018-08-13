5199f3a616c7a9f69c1ae6dac55a3f2309ece8cf
MerelyiGor <mogila1708@gmail.com>
2018-08-13 17:40:30 +0300

    custom class to curent menu item
    custom class to curent menu item

--- functions.php
+++ functions.php
@@ -79,6 +79,18 @@ function my_admin_term_filter() {
 }
 
 
+## Фильтр активных классов для меню.
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'custom-active-class-to-menu-item ';
+    }
+    return $classes;
+}
+
+
+
 /**
  * функция подключение (стили / скрипты) на все страницы
  * ---------------------------------------------------------------------------------------------------------------------
