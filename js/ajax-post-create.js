77f6acef659e54542d5eec3b64aba9848bf1a307
Merely_igor <mogila1708@gmail.com>
2019-02-08 01:13:23 +0200

    add js from ajax create post
    add js from ajax create post

---
+++ js/ajax-post-create.js
@@ -0,0 +1,32 @@
+// post_create_form -- reviews
+$('#modal-reviews form').submit(function (event) {
+    event.preventDefault();
+    let form = $(this),
+        form_data = new FormData();
+    // поля формы
+    form_data.append('action', 'post_add');
+    form_data.append('name', form.find('input[name="name"]').val());
+    form_data.append('mail', form.find('input[name="email"]').val());
+    form_data.append('textarea', form.find('textarea[name="msg"]').val());
+
+
+    $.ajax({
+        url: admin_ajax.url,
+        dataType: 'text',
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: form_data,
+        type: 'POST',
+        success: function (response) {
+        console.log('success', response);
+            //form.get(0).reset();
+            //let $popup = document.querySelector('#modal-getRequest');
+            //$.fancybox.open($popup);
+            //setTimeout(function () {
+            //    $.fancybox.close($popup);
+            //}, 5000);
+        }
+    });
+    console.log('form_data', form_data);
+});
