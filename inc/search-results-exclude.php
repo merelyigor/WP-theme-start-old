f87fd74783d04cac40697e99080658cd788b86cb
Merely_igor <mogila1708@gmail.com>
2019-03-07 16:41:53 +0200

    Modify the search
    Modify the search query with posts_where ACF filds

--- inc/search-results-exclude.php
+++ inc/search-results-exclude.php
@@ -79,6 +79,47 @@ function search_results_title() {
 }
 
 
+
+
+
+/**
+ * Modify the search query with posts_where ACF filds
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+ */
+function cf_search_where( $where ) {
+    global $pagenow, $wpdb;
+
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
+
+    return $where;
+}
+add_filter( 'posts_where', 'cf_search_where' );
+
+/**
+ * Prevent duplicates
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
+ */
+function cf_search_distinct( $where ) {
+    global $wpdb;
+
+    if ( is_search() ) {
+        return "DISTINCT";
+    }
+
+    return $where;
+}
+add_filter( 'posts_distinct', 'cf_search_distinct' );
+
+
+
+
+
 /**  -------------------- Весь список свойств которые можно использовать вместо условного тега
 $query->is_404
 $query->is_admin
