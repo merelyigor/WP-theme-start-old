300c09753fc9a193fc32aac03fcfbd9c73734b7e
Igor <mogila1708@gmail.com>
2019-09-07 13:49:28 +0300

    f

--- inc/always-included/my-custom-functions.php
+++ inc/always-included/my-custom-functions.php
@@ -3,7 +3,16 @@
 ## new var_dump function
 function dd($var_dump, $die = false)
 {
-    echo '<pre style="color: #850085">';
+    echo '<style>
+pre {
+    color: #850085
+}
+pre::selection {
+    color: #ff0; /* Цвет текста */
+    background: #000; /* Цвет фона */
+}
+  </style>';
+    echo '<pre>';
     var_dump($var_dump);
     echo '</pre>';
     if ($die) {
@@ -11,7 +20,7 @@ function dd($var_dump, $die = false)
     }
 }
 
-## valid this page url 
+## valid this page url
 function current_url()
 {
     $url      = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
